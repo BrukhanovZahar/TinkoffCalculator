4d7016867346f0ab04f50781cfd7f8beb44bc190
ZDBryukhanov <zdbryukhanov@mai.education>
2024-02-23 01:05:42 +0300

    add long press protocol

--- TinkoffCalculator/AppDelegate.swift
+++ TinkoffCalculator/AppDelegate.swift
@@ -7,10 +7,15 @@
 
 import UIKit
 
+protocol LongPressGestureAdder: UIGestureRecognizerDelegate {
+    func addGestureRecognizer()
+}
+
 @main
-class AppDelegate: UIResponder, UIApplicationDelegate {
-
-
+class AppDelegate: UIResponder, UIApplicationDelegate, LongPressGestureAdder {
+    
+    func addGestureRecognizer() {
+    }
 
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
         // Override point for customization after application launch.

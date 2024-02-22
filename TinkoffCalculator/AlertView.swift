4c5318a8a80964e67e4f21d856900b88e097a5f1
ZDBryukhanov <zdbryukhanov@mai.education>
2024-02-23 01:05:24 +0300

    add alert view

---
+++ TinkoffCalculator/AlertView.swift
@@ -0,0 +1,52 @@
+//
+//  AlertView.swift
+//  TinkoffCalculator
+//
+//  Created by Захар Брюханов on 23.02.2024.
+//
+
+import UIKit
+
+class AlertView: UIView {
+    var alertText: String? {
+        didSet {
+            label.text = alertText
+        }
+    }
+    
+    private let label: UILabel = {
+        let label = UILabel()
+        label.textColor = .white
+        label.textAlignment = .center
+        label.font = .systemFont(ofSize: 20)
+        return label
+    }()
+    
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+        setupUI()
+    }
+    
+    required init?(coder: NSCoder) {
+        super.init(coder: coder)
+        setupUI()
+    }
+    
+    private func setupUI() {
+        addSubview(label)
+        backgroundColor = .systemGreen
+        
+        let tap = UITapGestureRecognizer()
+        tap.addTarget(self, action: #selector(hide))
+        addGestureRecognizer(tap)
+    }
+    
+    @objc private func hide() {
+        removeFromSuperview()
+    }
+    
+    override func layoutSubviews() {
+        super.layoutSubviews()
+        label.frame = bounds
+    }
+}
